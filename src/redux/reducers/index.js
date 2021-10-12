26be8d244b48a174eb4fb8f080faeb63c8e390df
agungrizkyana <agung.rizkyana@gmail.com>
2021-10-12 17:24:32 +0700

    init basic code

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 
 import home from './home/slices';
-import todos from './todo/slices';
 
 const rootReducers = combineReducers({
   home,
-  todos,
 });
 
 export default rootReducers;
